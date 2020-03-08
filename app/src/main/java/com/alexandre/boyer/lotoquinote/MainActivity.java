5f64b6ddac424d975129722707b529b793dadaf7
Alexandre Boyer <alexandre.boyer@orange.fr>
2020-03-08 09:02:18 +0400

    Ajout d'un listener sur le bouton Delete pour supprimer les items sélectionnés dans la ListView

--- app/src/main/java/com/alexandre/boyer/lotoquinote/MainActivity.java
+++ app/src/main/java/com/alexandre/boyer/lotoquinote/MainActivity.java
@@ -9,6 +9,8 @@ import android.view.View;
 import android.widget.AbsListView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
@@ -24,6 +26,8 @@ public class MainActivity extends AppCompatActivity
 {
     ListView mListView;
     Button mNewDrawButton;
+    CheckBox mDrawCheckbox;
+    ImageButton mDeleteButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -33,6 +37,9 @@ public class MainActivity extends AppCompatActivity
 
         mListView = findViewById(R.id.listView);
         mNewDrawButton = findViewById(R.id.newDraw);
+        mDrawCheckbox = findViewById(R.id.activity_main_tirage_checkbox);
+        mDeleteButton = findViewById(R.id.deleteButton);
+
         final List<Tirage> draws = new ArrayList<>();
 
         mNewDrawButton.setOnClickListener(new View.OnClickListener()
@@ -59,6 +66,23 @@ public class MainActivity extends AppCompatActivity
                     mListView.setAdapter(adapter);
                 }
                 // Compter le nombre de tirages pour savoir quel numéro de tirage doit être crée
+            }
+        });
+
+        mDeleteButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                if(mDrawCheckbox.isChecked())
+                // /!\ ERREUR : java.lang.NullPointerException: Attempt to invoke virtual method 'boolean android.widget.CheckBox.isChecked()' on a null object reference
+                {
+                    int id = mDrawCheckbox.getId(); // Je ne sais pas si ça fonctionne correctement de récupérer l'Id
+                    // Je pense que si ça fonctionne, il faut stocker les Id dans un tableau pour gérer le cas où plusieurs Checkbox sont cochées
+
+                    // Suppression de l'item ou des items check de la ListView
+                    draws.remove(id);
+                }
             }
         });
     }

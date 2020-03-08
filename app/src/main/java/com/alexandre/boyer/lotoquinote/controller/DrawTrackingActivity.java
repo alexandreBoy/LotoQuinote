8896a5d096d69dcdaace2db35b0491b9529842d3
Alexandre Boyer <alexandre.boyer@orange.fr>
2020-03-08 15:58:07 +0400

    Ajout du listener sur le bouton Terminer de DrawTrackingActivity pour revenir au MainActivity

--- app/src/main/java/com/alexandre/boyer/lotoquinote/controller/DrawTrackingActivity.java
+++ app/src/main/java/com/alexandre/boyer/lotoquinote/controller/DrawTrackingActivity.java
@@ -2,17 +2,32 @@ package com.alexandre.boyer.lotoquinote.controller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.alexandre.boyer.lotoquinote.R;
 
 public class DrawTrackingActivity extends AppCompatActivity
 {
+    Button mFinishButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_draw_tracking);
+
+        mFinishButton = findViewById(R.id.activity_draw_finishButton);
+
+        mFinishButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                finish();
+            }
+        });
     }
 }

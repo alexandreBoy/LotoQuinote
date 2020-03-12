29c18bfa7dd59da8e901f049768e57a3f9e56aea
David Boisedu <david.boisedu@live.fr>
2020-03-12 11:00:33 +0400

    Mise en place des éléments graphiques pour la pop-up Modification/Suppression.

--- app/src/main/java/com/alexandre/boyer/lotoquinote/controller/ModifyPopUpActivity.java
+++ app/src/main/java/com/alexandre/boyer/lotoquinote/controller/ModifyPopUpActivity.java
@@ -2,7 +2,13 @@ package com.alexandre.boyer.lotoquinote.controller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+
+import com.alexandre.boyer.lotoquinote.R;
 
 public class ModifyPopUpActivity extends AppCompatActivity{
 
@@ -10,5 +16,17 @@ public class ModifyPopUpActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modify_pop_up);
+
+
+        Intent intent = getIntent();
+        intent.getSerializableExtra("currentDraw");
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int) (width*0.8),(int) (height*0.6));
     }
 }

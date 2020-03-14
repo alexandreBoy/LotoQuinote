eaeaf075e1460aadcb1de93643342aacb4eb4284
Alexandre Boyer <alexandre.boyer@orange.fr>
2020-03-14 09:59:05 +0400

    Creation de l'interface graphique DrawListActivity

---
+++ app/src/main/java/com/alexandre/boyer/lotoquinote/controller/DrawListActivity.java
@@ -0,0 +1,18 @@
+package com.alexandre.boyer.lotoquinote.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.alexandre.boyer.lotoquinote.R;
+
+public class DrawListActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_draw_list);
+    }
+}
